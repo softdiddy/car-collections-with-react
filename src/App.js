4ffe0d603a8af366321a497de321d57a052b1f21
Mohammed Lawal Abubakar <softdiddy@gmail.com>
2022-02-19 07:05:47 -0800

    setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import Header from './components/Header';
+import Mainbody from './components/Mainbody';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Mainbody />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Mainbody.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+function Mainbody() {
+  return (
+    <Container>
+        Mainbody
+    </Container>
+  )
+}
+
+export default Mainbody
+
+const Container=styled.div`
+  height: 100vh;
+`
